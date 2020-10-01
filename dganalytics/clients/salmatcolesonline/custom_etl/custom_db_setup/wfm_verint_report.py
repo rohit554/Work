96187ef2e6a51b6c5b54833463a7560417de75cc
Naga bandarupalli <naga.b@outlook.com>
2020-10-01 15:26:51 +1000

    probe fixes

---
+++ dganalytics/clients/salmatcolesonline/custom_etl/custom_db_setup/wfm_verint_report.py
@@ -0,0 +1,27 @@
+
+from dganalytics.utils.utils import get_spark_session, get_path_vars
+
+tenant = 'salmatcolesonline'
+tenant_path, db_path, log_path = get_path_vars(tenant)
+app_name = "salmatcolesonline_custom_wfm_setup"
+spark = get_spark_session(
+    app_name=app_name, tenant=tenant, default_db=f'dg_{tenant}')
+
+spark.sql(f"""
+        create table if not exists 
+            dg_{tenant}.wfm_verint_export
+            (
+                Date DATE,
+                Organisation string,
+                Employee string,
+                Supervisor string,
+                `Time Adhering toSchedule (Hours)` double,
+                `Time Not Adhering to Schedule (Hours)` double,
+                `Total Time Scheduled (Hours)` double,
+                `Time Adhering to Schedule (%)` double,
+                `Time Not Adhering to Schedule (%)` double,
+                `Adherence Violations` int
+            )
+            using delta
+            LOCATION '{db_path}/{tenant}/wfm_verint_export'
+        """)
---
+++ dganalytics/clients/salmatcolesonline/custom_etl/load_wfm/load_wfm.py
@@ -0,0 +1,46 @@
+from dganalytics.utils.utils import get_spark_session, get_path_vars
+import argparse
+import pandas as pd
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input_file', required=True)
+
+    args, unknown_args = parser.parse_known_args()
+    input_file = args.input_file
+
+    tenant = 'salmatcolesonline'
+    spark = get_spark_session('colesonline_load_wfm', tenant)
+    tenant_path, db_path, log_path = get_path_vars(tenant)
+    print("start loading WFM data for colesonline" + input_file)
+
+    df = pd.read_csv(
+        "/dbfs/mnt/datagamz/salmatcolesonline/data/raw/verint_wfm/" + input_file)
+
+    df = df.rename(columns={
+        "Date": "Date",
+        "Organisation": "Organisation",
+        "Employee": "Employee",
+        "Supervisor": "Supervisor",
+        "Time Adhering to Schedule (Hours)": "TimeAdheringToScheduleHours",
+        "Time Not Adhering to Schedule (Hours)": "TimeNotAdheringToScheduleHours",
+        "Total Time Scheduled (Hours)": "TotalTimeScheduledHours",
+        "Time Adhering to Schedule (%)": "TimeAdheringToSchedule",
+        "Time Not Adhering to Schedule (%)": "TimeNotAdheringToSchedule",
+        "Adherence Violations": "AdherenceViolations"
+    }, errors="raise")
+    df = df[['Date', 'Organisation', 'Employee', 'Supervisor', 'TimeAdheringToScheduleHours',
+             'TimeNotAdheringToScheduleHours', 'TotalTimeScheduledHours', 'TimeAdheringToSchedule',
+             'TimeNotAdheringToSchedule', 'AdherenceViolations']]
+    sdf = spark.createDataFrame(df)
+    sdf.registerTempTable("wfm")
+    spark.sql(f"""merge into dg_{tenant}.wfm_verint_export
+                using wfm
+                on wfm.Date = wfm_verint_export.Date
+                and wfm.Employee = wfm_verint_export.Employee
+                WHEN MATCHED THEN
+                    UPDATE SET *
+                WHEN NOT MATCHED THEN
+                    INSERT *
+                """)
+    print("loading finished")
