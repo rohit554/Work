c5c2688c921f0e0b7b23174ef8f0eb2ce23d8d31
Vara_Incresol <varaprasad.chodapaneedi@incresol.com>
2020-10-14 19:03:44 +0530

    azcopy changes

--- devops_utils/powershell/azcopyfoldertoblob.ps1
+++ devops_utils/powershell/azcopyfoldertoblob.ps1
@@ -8,20 +8,20 @@ param
     [String] [Parameter(Mandatory = $true)] $storageKey
 )
 #Download AzCopy
-wget https://aka.ms/downloadazcopy-v10-linux
+# wget https://aka.ms/downloadazcopy-v10-linux
 
 #Expand Archive
-tar -xvf downloadazcopy-v10-linux
+# tar -xvf downloadazcopy-v10-linux
 
 #(Optional) Remove existing AzCopy version
-sudo rm /usr/bin/azcopy
+# sudo rm /usr/bin/azcopy
 
 # Move AzCopy to the destination you want to store it
-sudo cp ./azcopy_linux_amd64_*/azcopy /usr/bin/
+# sudo cp ./azcopy_linux_amd64_*/azcopy /usr/bin/
 $simpleblobconnector = 'https://' +$storageaccountname + '.blob.core.windows.net/' + $blobcontainername
 $finalblobconnector = 'https://' +$storageaccountname + '.blob.core.windows.net/' + $blobcontainername + '/' +$blobcontainerpath
 #azcopy --version
 #azcopy --help
 #azcopy -h
 #azcopy login
-azcopy copy --source $uploaddirectory --destination $finalblobconnector  --dest-key $storageKey --recursive --include-pattern="*.py;*.json;*txt;*.in" --exclude-path=".venv;build;devops_utils;dganalytics.egg-info;dist;.vscode" --exclude-pattern="*.pyc"
+azcopy --source $uploaddirectory --destination $finalblobconnector  --dest-key $storageKey --recursive --include-pattern="*.py;*.json;*txt;*.in" --exclude-path=".venv;build;devops_utils;dganalytics.egg-info;dist;.vscode" --exclude-pattern="*.pyc"
