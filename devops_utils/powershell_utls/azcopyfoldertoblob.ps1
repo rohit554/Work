fe8c9469f1aa6fd002c23d74b6815eb9dae352c8
Vara_Incresol <varaprasad.chodapaneedi@incresol.com>
2020-10-14 18:37:43 +0530

    added CI pipeline and required utils

--- devops_utils/powershell_utls/azcopyfoldertoblob.ps1
+++ devops_utils/powershell_utls/azcopyfoldertoblob.ps1
@@ -1,28 +1,27 @@
 [CmdletBinding(DefaultParameterSetName = 'None')]
 param
 (
-  [String] [Parameter(Mandatory = $true)] $uploaddirectory,
-  [String] [Parameter(Mandatory = $true)] $storageaccountname,
-  [String] [Parameter(Mandatory = $true)] $blobcontainername,
-  [String] [Parameter(Mandatory = $true)] $blobcontainerpath,
-  [String] [Parameter(Mandatory = $true)] $storageKey
-  
+    [String] [Parameter(Mandatory = $true)] $uploaddirectory,
+    [String] [Parameter(Mandatory = $true)] $storageaccountname,
+    [String] [Parameter(Mandatory = $true)] $blobcontainername,
+    [String] [Parameter(Mandatory = $true)] $blobcontainerpath,
+    [String] [Parameter(Mandatory = $true)] $storageKey
 )
 #Download AzCopy
-#wget https://aka.ms/downloadazcopy-v10-linux
- 
+wget https://aka.ms/downloadazcopy-v10-linux
+
 #Expand Archive
-#tar -xvf downloadazcopy-v10-linux
- 
+tar -xvf downloadazcopy-v10-linux
+
 #(Optional) Remove existing AzCopy version
-#sudo rm /usr/bin/azcopy
- 
-#Move AzCopy to the destination you want to store it
-#sudo cp ./azcopy_linux_amd64_*/azcopy /usr/bin/
+sudo rm /usr/bin/azcopy
+
+# Move AzCopy to the destination you want to store it
+sudo cp ./azcopy_linux_amd64_*/azcopy /usr/bin/
 $simpleblobconnector = 'https://' +$storageaccountname + '.blob.core.windows.net/' + $blobcontainername
 $finalblobconnector = 'https://' +$storageaccountname + '.blob.core.windows.net/' + $blobcontainername + '/' +$blobcontainerpath
 #azcopy --version
 #azcopy --help
 #azcopy -h
 #azcopy login
-azcopy --source $uploaddirectory --destination $finalblobconnector  --dest-key $storageKey --recursive --include-pattern="*.py;*.json;*txt;*.in" --exclude-path=".venv;build;dganalytics.egg-info;dist;.vscode" --exclude-pattern="*.pyc"
+azcopy copy --source $uploaddirectory --destination $finalblobconnector  --dest-key $storageKey --recursive --include-pattern="*.py;*.json;*txt;*.in" --exclude-path=".venv;build;devops_utils;dganalytics.egg-info;dist;.vscode" --exclude-pattern="*.pyc"
