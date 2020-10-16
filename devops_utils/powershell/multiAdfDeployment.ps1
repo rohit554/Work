286ae0a05a91b8e8789f19938dbad955fb586a3f
Vara_Incresol <varaprasad.chodapaneedi@incresol.com>
2020-10-16 19:59:14 +0530

    removing echo

--- devops_utils/powershell/multiAdfDeployment.ps1
+++ devops_utils/powershell/multiAdfDeployment.ps1
@@ -10,11 +10,11 @@ param
 $jsondata = Get-Content -Raw -Path $DeploymentJsonFile | ConvertFrom-Json
 $jsondata.$tenant.adf.PSobject.Properties  | ForEach-Object {
     $pipelinepath = $currntDirectory +"/"+ $($_.Value).pipeline
-    $deploymentString = " New-AzResourceGroupDeployment -ResourceGroupName " + $resourceGroup +" -TemplateFile " + $pipelinepath + " -env" + $env
+    $deploymentString = " New-AzResourceGroupDeployment -ResourceGroupName " + $resourceGroup +" -TemplateFile " + $pipelinepath + " -env " + $env
     $($_.Value).parameters.PSobject.Properties |  ForEach-Object {
         # if ( $($_.Name) -NotIn 'env' ,'tenant') {
         $deploymentString = $deploymentString + " -$($_.Name) $($_.Value)"
         # }
     }
-    Invoke-Expression("echo $deploymentString" )
+    Invoke-Expression($deploymentString )
 }
