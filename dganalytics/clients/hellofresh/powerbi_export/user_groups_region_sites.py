fe34034a9b06ba3c91e02f93f068027acf14f936
Naga bandarupalli <naga.b@outlook.com>
2021-01-20 23:47:14 +1100

    HF write realtime configs as part of pipeline

--- dganalytics/clients/hellofresh/powerbi_export/user_groups_region_sites.py
+++ dganalytics/clients/hellofresh/powerbi_export/user_groups_region_sites.py
@@ -35,5 +35,20 @@ def export_user_groups_region_sites(spark: SparkSession, tenant: str, region: st
                                        header=True, index=False)
     df = user_group_sites.selectExpr(
         "userId as userKey", "groupName", "region", "site", "timeZone as time_zone")
+    
+    realtime_config = spark.sql("""
+        select a.groupName, a.userId, c.userFullName name from (
+	select 
+	a.userId, a.groupName groupName, b.region, b.site, b.timeZone, 
+	row_number() over(partition by a.userId order by a.groupName) as rn
+	from gpc_hellofresh.dim_user_groups a, group_timezones b, gpc_hellofresh.dim_users c
+	where lower(a.groupName)  = lower(b.agentGroupName)
+    and a.userId = c.userId
+    and lower(b.region) = 'us'
+	) where rn= 1
+
+        """).toPandas()
+    realtime_config.to_csv(os.path.join(tenant_path, 'data', 'config', 'realtime_US_Users.csv'),
+                           header=True, index=False)
 
     return df
--- dganalytics/clients/hellofresh/powerbi_export/wrapup_codes.py
+++ dganalytics/clients/hellofresh/powerbi_export/wrapup_codes.py
@@ -1,9 +1,17 @@
 from pyspark.sql import SparkSession
+from dganalytics.utils.utils import get_path_vars
+import os
+
 
 def export_wrapup_codes(spark: SparkSession, tenant: str, region: str):
 
     df = spark.sql("""
             select wrapupId as wapupCodeKey, wrapupCode as wrapupDescription from gpc_hellofresh.dim_wrapup_codes
     """)
-
+    tenant_path, db_path, log_path = get_path_vars('hellofresh')
+    realtime_config = spark.sql("""
+            select distinct wrapupId as wrapup_code_id, wrapupCode as wrapup_description from gpc_hellofresh.dim_wrapup_codes
+    """).toPandas()
+    realtime_config.to_csv(os.path.join(tenant_path, 'data', 'config', 'realtime_wrapupup_codes.csv'),
+                           header=True, index=False)
     return df
