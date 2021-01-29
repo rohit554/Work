26c89fe954419f4b58ab763772f89dfa119fba05
Saurabh Pandit <saurabh.pandit@innablr.com.au>
2021-01-29 19:45:02 +1100

    Restoring dganalytics/ssis_replacement/ from a4307fe151074151e843a2e4943dd7e428217175
    Signed-off-by: Saurabh Pandit <saurabh.pandit@innablr.com.au>

---
+++ dganalytics/ssis_replacement/__init__.py
@@ -0,0 +1,12 @@
+# from .tenants import config
+from .helpers import utils
+from .helpers import mongo_utils
+
+
+from dganalytics.clients import holden
+from dganalytics.clients import vodafoneqatar
+
+config = {
+    'holden': holden.ssis_replacement_config,
+    'vodafoneqatar': vodafoneqatar.ssis_replacement_config,
+}
---
+++ dganalytics/ssis_replacement/helpers/mongo_utils.py
@@ -0,0 +1,71 @@
+from dganalytics.utils.utils import get_mongo_conxn
+
+def aggregate_mongo_colxn(
+    mongodb_conxnx_uri,
+    database_name,
+    collection_name,
+    aggr_pipeline,
+    ):
+    with get_mongo_conxn( mongodb_conxnx_uri ) as mongodb_cluster_client:
+        database_connxn = mongodb_cluster_client.get_database(database_name)
+        collection_connxn = database_connxn.get_collection(collection_name)
+        try:
+            out = collection_connxn.aggregate(aggr_pipeline)
+        except Exception as e:
+            raise Exception(
+                f"Excepton {e} occured. Please check mongodb_conxnx_uri, aggregation pipline"
+            )
+    return out
+
+
+def upsertone_mongo_colxn(
+    mongodb_conxnx_uri,
+    database_name,
+    collection_name,
+    upsert_filter,
+    update,
+    ):
+    with get_mongo_conxn( mongodb_conxnx_uri ) as mongodb_cluster_client:
+        database_connxn = mongodb_cluster_client.get_database(database_name)
+        collection_connxn = database_connxn.get_collection(collection_name)
+        try:
+            out = collection_connxn.update_one( filter=upsert_filter, update= update,upsert=True)
+        except Exception as e:
+            raise Exception(
+                f"Excepton {e} occured. Please check mongodb_conxnx_uri, duplication, filter, update doc"
+            )
+    return out
+
+def drop_mongo_colxn(
+    mongodb_conxnx_uri,
+    database_name,
+    collection_name,
+    ):
+    with get_mongo_conxn( mongodb_conxnx_uri ) as mongodb_cluster_client:
+        database_connxn = mongodb_cluster_client.get_database(database_name)
+        collection_connxn = database_connxn.get_collection(collection_name)
+        try:
+            out = collection_connxn.drop()
+        except Exception as e:
+            raise Exception(
+                f"Excepton {e} occured. Please check mongodb_conxnx_uri, dbname, collection, other relavant details"
+            )
+    return out
+
+
+def get_config_mongo(
+    mongodb_conxnx_uri,
+    database_name,
+    collection_name,
+    find_filter,
+    ):
+    with get_mongo_conxn( mongodb_conxnx_uri ) as mongodb_cluster_client:
+        database_connxn = mongodb_cluster_client.get_database(database_name)
+        collection_connxn = database_connxn.get_collection(collection_name)
+        try:
+            out = collection_connxn.find(find_filter)
+        except Exception as e:
+            raise Exception(
+                f"Excepton {e} occured. Please check mongodb_conxnx_uri, dbname, collection, other relavant details"
+            )
+    return out
