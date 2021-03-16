31c6f7e3d9e26efbe9ee69528f7f31785ac0ce2e
Saurabh Pandit <958948+saurabhpandit@users.noreply.github.com>
2021-03-16 20:55:44 +1100

    Iterating over orgs to fetch user's levels data
    Signed-off-by: Saurabh Pandit <958948+saurabhpandit@users.noreply.github.com>

--- dganalytics/performance_management_report/queries/levels.py
+++ dganalytics/performance_management_report/queries/levels.py
@@ -1,150 +1,113 @@
 from dganalytics.utils.utils import exec_mongo_pipeline, delta_table_partition_ovrewrite
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType, TimestampType
 
-pipeline = [
-        { 
-            "$unwind" : { 
-                "path" : "$campaign_levels"
-            }
-        }, 
-        { 
-            "$project" : { 
-                "user_id" : 1.0, 
-                "campaign_id" : "$campaign_levels.campaign_id", 
-                "level_id" : "$campaign_levels.level", 
-                "achieved_date" : "$campaign_levels.achieved_date", 
-                "org_id" : 1.0
-            }
-        }, 
-        { 
-            "$lookup" : { 
-                "from" : "Campaign", 
-                "localField" : "campaign_id", 
-                "foreignField" : "_id", 
-                "as" : "campaigns"
-            }
-        }, 
-        { 
-            "$unwind" : { 
-                "path" : "$campaigns", 
-                "includeArrayIndex" : "arrayIndex", 
-                "preserveNullAndEmptyArrays" : False
-            }
-        }, 
-        { 
-            "$project" : { 
-                "user_id" : 1.0, 
-                "campaign_id" : 1.0, 
-                "level_id" : 1.0, 
-                "achieved_date" : 1.0, 
-                "campaign_name" : "$campaigns.name", 
-                "campaign_level" : "$campaigns.levels", 
-                "org_id" : 1.0
-            }
-        }, 
-        { 
-            "$unwind" : { 
-                "path" : "$campaign_level", 
-                "includeArrayIndex" : "arrayIndex", 
-                "preserveNullAndEmptyArrays" : False
-            }
-        }, 
-        { 
-            "$match" : { 
-                "$expr" : { 
-                    "$eq" : [
-                        "$level_id", 
-                        "$campaign_level._id"
-                    ]
+def build_pipeline(org_id: str, org_timezone: str):
+    pipeline = [
+            {
+                "$match": {
+                    "$expr": {
+                        "$eq": [{"$strcasecmp": ["$org_id", org_id]}, 0]
+                    },
                 }
-            }
-        }, 
-        { 
-            "$project" : { 
-                "_id" : 0.0, 
-                "mongo_user_id" : "$_id", 
-                "user_id" : 1.0, 
-                "campaign_id" : 1.0, 
-                "level_id" : 1.0, 
-                "achieved_date" : 1.0, 
-                "campaign_name" : 1.0, 
-                "level_name" : "$campaign_levels.name", 
-                "level_start_points" : "$campaign_level.start_points", 
-                "level_end_points" : "$campaign_level.end_points", 
-                "level_number" : "$campaign_level.id", 
-                "org_id" : 1.0
-            }
-        }, 
-        { 
-            "$lookup" : { 
-                "from" : "Organization", 
-                "let" : { 
-                    "oid" : "$org_id"
-                }, 
-                "pipeline" : [
-                    { 
-                        "$match" : { 
-                            "$expr" : { 
-                                "$and" : [
-                                    { 
-                                        "$eq" : [
-                                            "$org_id", 
-                                            "$$oid"
-                                        ]
-                                    }, 
-                                    { 
-                                        "$eq" : [
-                                            "$type", 
-                                            "Organisation"
-                                        ]
-                                    }
-                                ]
+            }, 
+            { 
+                "$unwind" : { 
+                    "path" : "$campaign_levels"
+                }
+            }, 
+            { 
+                "$project" : { 
+                    "user_id" : 1.0, 
+                    "campaign_id" : "$campaign_levels.campaign_id", 
+                    "level_id" : "$campaign_levels.level", 
+                    "achieved_date" : "$campaign_levels.achieved_date", 
+                    "org_id" : 1.0
+                }
+            }, 
+            { 
+                "$lookup" : { 
+                    "from" : "Campaign", 
+                    "localField" : "campaign_id", 
+                    "foreignField" : "_id", 
+                    "as" : "campaigns"
+                }
+            }, 
+            { 
+                "$unwind" : { 
+                    "path" : "$campaigns", 
+                    "includeArrayIndex" : "arrayIndex", 
+                    "preserveNullAndEmptyArrays" : False
+                }
+            }, 
+            { 
+                "$project" : { 
+                    "user_id" : 1.0, 
+                    "campaign_id" : 1.0, 
+                    "level_id" : 1.0, 
+                    "achieved_date" : 1.0, 
+                    "campaign_name" : "$campaigns.name", 
+                    "campaign_level" : "$campaigns.levels", 
+                    "org_id" : 1.0
+                }
+            }, 
+            { 
+                "$unwind" : { 
+                    "path" : "$campaign_level", 
+                    "includeArrayIndex" : "arrayIndex", 
+                    "preserveNullAndEmptyArrays" : False
+                }
+            }, 
+            { 
+                "$match" : { 
+                    "$expr" : { 
+                        "$eq" : [
+                            "$level_id", 
+                            "$campaign_level._id"
+                        ]
+                    }
+                }
+            }, 
+            { 
+                "$project" : { 
+                    "_id" : 0.0, 
+                    "mongo_user_id" : "$_id", 
+                    "user_id" : 1.0, 
+                    "campaign_id" : 1.0, 
+                    "level_id" : 1.0, 
+                    "achieved_date" : 1.0, 
+                    "campaign_name" : 1.0, 
+                    "level_name" : "$campaign_levels.name", 
+                    "level_start_points" : "$campaign_level.start_points", 
+                    "level_end_points" : "$campaign_level.end_points", 
+                    "level_number" : "$campaign_level.id", 
+                    "org_id" : 1.0
+                }
+            },
+            { 
+                "$project" : { 
+                    "mongo_user_id" : 1.0, 
+                    "user_id" : 1.0, 
+                    "campaign_id" : 1.0, 
+                    "level_id" : 1.0, 
+                    "achieved_date" : { 
+                        "$toDate" : { 
+                            "$dateToString" : { 
+                                "date" : "$achieved_date", 
+                                "timezone" : org_timezone
                             }
                         }
                     }, 
-                    { 
-                        "$project" : { 
-                            "timezone" : { 
-                                "$ifNull" : [
-                                    "$timezone", 
-                                    "Australia/Melbourne"
-                                ]
-                            }
-                        }
-                    }
-                ], 
-                "as" : "org"
+                    "campaign_name" : 1.0, 
+                    "level_name" : 1.0, 
+                    "level_start_points" : 1.0, 
+                    "level_end_points" : 1.0, 
+                    "level_number" : 1.0, 
+                    "org_id" : 1.0
+                }
             }
-        }, 
-        { 
-            "$unwind" : { 
-                "path" : "$org", 
-                "preserveNullAndEmptyArrays" : True
-            }
-        }, 
-        { 
-            "$project" : { 
-                "mongo_user_id" : 1.0, 
-                "user_id" : 1.0, 
-                "campaign_id" : 1.0, 
-                "level_id" : 1.0, 
-                "achieved_date" : { 
-                    "$toDate" : { 
-                        "$dateToString" : { 
-                            "date" : "$achieved_date", 
-                            "timezone" : "$org.timezone"
-                        }
-                    }
-                }, 
-                "campaign_name" : 1.0, 
-                "level_name" : 1.0, 
-                "level_start_points" : 1.0, 
-                "level_end_points" : 1.0, 
-                "level_number" : 1.0, 
-                "org_id" : 1.0
-            }
-        }
-    ]
+        ]
+
+    return pipeline
 
 schema = StructType([StructField('achieved_date', TimestampType(), True),
                      StructField('campaign_id', StructType(
@@ -160,9 +123,63 @@ schema = StructType([StructField('achieved_date', TimestampType(), True),
                          [StructField('oid', StringType(), True)]), True),
                      StructField('user_id', StringType(), True)])
 
+org_timezone_schema = StructType([
+    StructField('timezone', StringType(), False)
+])
 
 def get_levels(spark):
-    df = exec_mongo_pipeline(spark, pipeline, 'User', schema)
+    org_id_rows = spark.sql(
+        "select distinct orgId from dg_performance_management.campaign order by orgId"
+    ).select("orgId").collect()
+
+    df = None
+
+    # Get campaign & challenges for each org
+
+    for org_id_row in org_id_rows:
+        org_id = org_id_row.asDict()['orgId']
+        org_timezone_pipeline = [{
+            "$match": {
+                "$expr": {
+                    "$and": [{
+                        "$eq": [{
+                            "$strcasecmp": ["$org_id", org_id]
+                        }, 0]
+                    }, {
+                        "$eq": ["$type", "Organisation"]
+                    }, {
+                        "$eq": ["$is_active", True]
+                    }, {
+                        "$eq": ["$is_deleted", False]
+                    }]
+                }
+            }
+        }, {
+            "$project": {
+                "org_id": 1.0,
+                "timezone": {
+                    "$ifNull": ["$timezone", "Australia/Melbourne"]
+                }
+            }
+        }]
+
+        org_timezone_row = exec_mongo_pipeline(
+            spark, org_timezone_pipeline, 'Organization', 
+            org_timezone_schema).select("timezone").first()
+
+        if not org_timezone_row:
+            continue
+
+        org_timezone = org_timezone_row.asDict()['timezone']
+        pipeline = build_pipeline(org_id, org_timezone)
+        
+        levels_df = exec_mongo_pipeline(spark, pipeline, 'User', schema)
+
+        if df is None:
+            df = levels_df
+        else:
+            df = df.union(levels_df)
+
     df.registerTempTable("levels")
     df = spark.sql("""
                     select  distinct achieved_date achievedDate,
