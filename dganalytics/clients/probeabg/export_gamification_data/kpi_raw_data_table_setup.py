aeae9b4fa282ee884ad6315cdf9e87d578161069
Saurabh Pandit <958948+saurabhpandit@users.noreply.github.com>
2021-03-31 20:53:22 +1100

    DG-611 Duplicate code for probeabg client which matches the profile of salmatcolesonline
    Signed-off-by: Saurabh Pandit <958948+saurabhpandit@users.noreply.github.com>

---
+++ dganalytics/clients/probeabg/export_gamification_data/kpi_raw_data_table_setup.py
@@ -0,0 +1,47 @@
+from dganalytics.utils.utils import get_spark_session, get_path_vars
+tenant = 'probeabg'
+app_name = "gpc_dg_metadata_probeabg_export"
+tenant_path, db_path, log_path = get_path_vars(tenant)
+spark = get_spark_session(app_name, tenant, default_db='dg_probeabg')
+
+spark.sql(f"""
+            create table if not exists dg_probeabg.kpi_raw_data
+                (
+                    userId string,
+                    date date,
+                    DailyAdherencePercentage float,
+                    SumDailyQAScoreVoice float,
+                    CountDailyQAScoreVoice int,
+                    SumDailyQAScoreMessage float,
+                    CountDailyQAScoreMessage int,
+    	            SumDailyQAScoreEmail float,
+                    CountDailyQAScoreEmail int,
+                    SumDailyQAScore float,
+                    CountDailyQAScore int,
+                    SumDailyHoldTimeVoice float,
+                    CountDailyHoldTimeVoice int,
+    	            SumDailyHoldTimeMessage float,
+                    CountDailyHoldTimeMessage int,
+                    SumDailyHoldTimeEmail float,
+                    CountDailyHoldTimeEmail int,
+                    SumDailyHoldTime float,
+                    CountDailyHoldTime int,
+                    SumDailyAcwTimeVoice float,
+                    CountDailyAcwTimeVoice int,
+                    SumDailyAcwTimeMessage float,
+                    CountDailyAcwTimeMessage int,
+		            SumDailyAcwTimeEmail float,
+                    CountDailyAcwTimeEmail int,
+                    SumDailyAcwTime float,
+                    CountDailyAcwTime int,
+                    SumDailyNotRespondingTime float,
+                    CountDailyHandleChat int,
+                    CountDailyHandleEmail int,
+                    CountDailyHandleVoice int,
+                    CountDailyHandleMessage int,
+                    CountDailyHandle int
+                )
+                using delta
+                partitioned by(date)
+                LOCATION '{db_path}/dg_probeabg/kpi_raw_data'
+            """)
