17116ad1733217ad8e3046b3658f3d867c5d4fff
Abhinay Gorghate <abhinay@datagamz.com>
2022-11-21 13:15:01 +0530

    Exporting All SFTP Data In Additional SFTP

--- dganalytics/clients/simplyenergy/export_data/simplyenergy_sftp_export.py
+++ dganalytics/clients/simplyenergy/export_data/simplyenergy_sftp_export.py
@@ -14,13 +14,22 @@ if __name__ == "__main__":
     logger.info(f"starting {tenant} sftp export")
     parser = argparse.ArgumentParser()
     parser.add_argument('--extract_start_time', required=True,
-                        type=lambda s: datetime.strptime(s, '%Y-%m-%dT%H:%M:%SZ'))
+                      type=lambda s: datetime.strptime(s, '%Y-%m-%dT%H:%M:%SZ'))
     parser.add_argument('--extract_end_time', required=True, type=lambda s: datetime.strptime(s, '%Y-%m-%dT%H:%M:%SZ'))
     parser.add_argument('--export_name', required=True)
+    parser.add_argument('--sftp_host', required=True)
+    parser.add_argument('--sftp_username', required=True)
+    parser.add_argument('--sftp_password', required=True)
+    parser.add_argument('--sftp_folder_path', required=False)
     args, unknown_args = parser.parse_known_args()
     extract_start_time = args.extract_start_time.strftime('%Y-%m-%dT%H:%M:%S')
     extract_end_time = args.extract_end_time.strftime('%Y-%m-%dT%H:%M:%S')
     export_name = args.export_name
+    sftp_host = args.sftp_host
+    sftp_username = args.sftp_username
+    sftp_password = args.sftp_password
+    sftp_folder_path = args.sftp_folder_path
+
     fact_export_type = export_name.startswith("fact_")
 
     if export_name.startswith("fact_"):
@@ -50,9 +59,9 @@ if __name__ == "__main__":
         df.write.option("escape", "\"").option("multiline", "true").csv(path=export_path_df, mode="overwrite", sep=",", header=True, dateFormat="yyyy-MM-dd",
                      timestampFormat="yyyy-MM-dd HH:mm:ss", encoding="utf-8")
 
-        sftp_host = get_secret(f"{tenant}SFTPHost")
-        sftp_username = get_secret(f"{tenant}SFTPUsername")
-        sftp_password = get_secret(f"{tenant}SFTPPassword")
+        sftp_host = get_secret(sftp_host)
+        sftp_username = get_secret(sftp_username)
+        sftp_password = get_secret(sftp_password)
         sftp_cn_opts = pysftp.CnOpts()
         sftp_cn_opts.hostkeys = None
 
@@ -61,19 +70,23 @@ if __name__ == "__main__":
         with pysftp.Connection(host=sftp_host, username=sftp_username, password=sftp_password,
                                cnopts=sftp_cn_opts) as sftp_conn:
             export_part = 0
-
             for file_name in sorted(os.listdir(export_path_os)):
                 file_path = f"{export_path_os}/{file_name}"
-
                 if not os.path.isfile(file_path) or not file_path.lower().endswith(".csv"):
                     os.remove(file_path)
                     continue
 
                 if export_name.startswith("fact_"):
                     export_part += 1
-                    upload_name = f"./{export_name_normalised}_{export_part}.csv"
+                    if sftp_folder_path == "" or sftp_folder_path is None:
+                        upload_name = f"./{export_name_normalised}_{export_part}.csv"
+                    else: 
+                      upload_name = f"./{sftp_folder_path}/{export_name_normalised}_{export_part}.csv"
                 else:
-                    upload_name = f"./{export_name_normalised}.csv"
+                    if sftp_folder_path == "" or sftp_folder_path is None:
+                        upload_name = f"./{export_name_normalised}.csv"
+                    else: 
+                      upload_name = f"./{sftp_folder_path}/{export_name_normalised}.csv"
 
                 logger.info(f"uploading {file_path} to {upload_name}...")
                 sftp_conn.put(file_path, upload_name)
