ad86d1710172be86d3549459d7659dac8080718a
AshishKumbhara <akumbhar@datagamz.com>
2023-09-06 16:01:15 +0530

    push_gamification updated query and changes in the json file like to renaming the earlier values

--- dganalytics/clients/skynz/export_gamification_data/push_gamification.py
+++ dganalytics/clients/skynz/export_gamification_data/push_gamification.py
@@ -8,57 +8,79 @@ from datetime import datetime, timedelta
 
 if __name__ == "__main__":
     org_id = 'skynzib'
-    back_days = 3
     df = spark.sql(f"""
-        WITH UserDates AS (
-            SELECT
-            u.userId,
-            date_format(D._date, 'dd-MM-yyyy') AS _date
-            FROM
-            gpc_skynz.dim_users u
-            CROSS JOIN (
-                SELECT
-                explode(
-                    sequence(
-                    CAST(current_date() AS DATE) - {back_days},
-                    CAST(current_date() AS DATE),
-                    interval 1 day
-                    )
-                ) _date
-            ) AS D
-            WHERE
-            u.state = 'active'
-        ),
-        FW AS (
-            SELECT
-            U.userId,
-            U._date,
-            SUM(D.adherenceScheduleSecs) AS adherenceScheduleSecs,
-            SUM(D.exceptionDurationSecs) AS exceptionDurationSecs
-            FROM
-            gpc_skynz.fact_wfm_day_metrics D
-            JOIN UserDates U ON U.userId = D.userId
-            AND date_format(
-                from_utc_timestamp(D.startDate, 'Pacific/Auckland'),
-                'dd-MM-yyyy'
-            ) = U._date
-            GROUP BY
-            U.userId,
-            _date
-        )
+        WITH maxScore AS (
+    SELECT
+        questionGroupId,
+        questionGroupName,
+        questionId,
+        MAX(answerOption.value) AS maxScore
+    FROM (
         SELECT
-        UD.userId,
-        UD._date AS Date,
-        FW.adherenceScheduleSecs AS adherenceScheduleSecs,
-        FW.exceptionDurationSecs AS exceptionDurationSecs
-        FROM
-            UserDates UD
-            LEFT JOIN FW ON FW.userId = UD.userId
-            AND FW._date = UD._date
-        WHERE
-            NOT (
-            COALESCE(FW.adherenceScheduleSecs, 0) = 0
+            questionGroupId,
+            questionGroupName,
+            question.id AS questionId,
+            EXPLODE(question.answerOptions) AS answerOption
+        FROM (
+            SELECT
+                questionGroup.id AS questionGroupId,
+                questionGroup.name AS questionGroupName,
+                EXPLODE(questionGroup.questions) AS question
+            FROM (
+                SELECT EXPLODE(questionGroups) AS questionGroup
+                FROM gpc_skynz.raw_evaluation_forms
+                WHERE name LIKE 'IB Sales CC-RAW '
+                OR name = 'OB Sales CC-RAW'
             )
+            WHERE questionGroup.name IN (
+                'Step 1: Create Connection',
+                'Step 5: Wrap',
+                'Step 2: Confirm Need',
+                'Step 4.  Add Value'
+            )
+        )
+    )
+    GROUP BY
+        questionGroupId,
+        questionGroupName,
+        questionId
+)
+
+        SELECT
+            m.agentId AS userId,
+            m.assignedDate,
+            `Step 1: Create Connection` AS `QA Score - Create Connection`,
+            `Step 2: Confirm Need` AS `QA Score - Confirm Need`,
+            `Step 4.  Add Value` AS `QA Score - Add Value`,
+            `Step 5: Wrap` AS `QA Score - Wrap`,
+            ts.totalScore AS Overall_Score
+        FROM (
+            SELECT
+                e.agentId,
+                e.assignedDate,
+                e.evaluationId,
+                ms.questionGroupName,
+                (SUM(eqs.score) * 100 / SUM(ms.maxScore)) AS groupScore
+            FROM gpc_skynz.fact_evaluation_question_scores eqs
+            JOIN maxScore ms 
+            ON eqs.questionId = ms.questionId
+            JOIN gpc_skynz.dim_evaluations e 
+            ON e.evaluationId = eqs.evaluationId
+            WHERE NOT eqs.markedNA
+            GROUP BY e.agentId, e.assignedDate, e.evaluationId, ms.questionGroupId, ms.questionGroupName
+        ) AS m
+        JOIN gpc_skynz.fact_evaluation_total_scores ts 
+        ON m.evaluationId = ts.evaluationId
+        PIVOT (
+            SUM(COALESCE(groupScore, 0))
+            FOR questionGroupName IN (
+                'Step 1: Create Connection',
+                'Step 2: Confirm Need',
+                'Step 4.  Add Value',
+                'Step 5: Wrap'
+            )
+        )
+
     """)
 
     push_gamification_data(df.toPandas(), org_id.upper(), 'SKYIB_Adherence_Connection')
