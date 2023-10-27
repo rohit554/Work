103f4a95f0712effec6bd30d31d0460e9bb94452
Ganga_C <gchapalli@datagamz.com>
2023-10-27 22:27:36 +1100

    ci-206 roi incremental load announcements, campaign activities and kpis

--- dganalytics/performance_management_report/queries/campaign_activities.py
+++ dganalytics/performance_management_report/queries/campaign_activities.py
@@ -1,40 +1,59 @@
 from pyspark.sql.types import StructType, StructField, StringType, BooleanType
-from dganalytics.utils.utils import exec_mongo_pipeline, delta_table_partition_ovrewrite
+from dganalytics.utils.utils import exec_mongo_pipeline, delta_table_partition_ovrewrite, get_active_org
 from datetime import datetime, timedelta
-from dganalytics.utils.utils import get_spark_session
 
-pipeline = [{
-        '$project': {
-            'campaign_id': 1, 
-            'activityId': '$_id', 
-            'entityName' : '$entity_name',
-            'fieldName': '$entity_column_name', 
-            'kpi_name': 1,
-            'orgId': "$org_id"
-            }
-    }
-]
 
-schema = StructType([StructField('campaign_id', StructType(
-                         [StructField('oid', StringType(), True)]), True),
-                    StructField('activityId', StructType(
-						 [StructField('oid', StringType(), True)]), True),
+schema = StructType([StructField('campaignId', StringType(), True),
+                    StructField('activityId', StringType(), True),
                     StructField('entityName', StringType(), True),
                     StructField('fieldName', StringType(), True),
                     StructField('kpi_name', StringType(), True),
                     StructField('orgId', StringType(), True)])
 
 def get_campaign_activities(spark):
+  extract_start_time = (datetime.now() - timedelta(days=3)).strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+  for tenant in get_active_org():
+    pipeline = [
+      {
+        "$match":{
+                "org_id": tenant,
+                '$expr': {
+                    '$or': [
+                        {
+                            '$gte': [
+                                '$creation_date', { '$date': extract_start_time }
+                            ]
+                        }, {
+                            '$gte': [
+                                '$modified_date', { '$date': extract_start_time }
+                            ]
+                        }
+                    ]
+                }
+                
+            }
+      },
+      {
+            '$project': {
+                'campaignId' : '$campaign_id', 
+                'activityId': '$_id', 
+                'entityName' : '$entity_name',
+                'fieldName': '$entity_column_name', 
+                'kpi_name': 1,
+                'orgId': "$org_id"
+                }
+        }
+    ]
     outcomes = exec_mongo_pipeline(spark, pipeline, 'Outcomes', schema)
+    outcomes = outcomes.withColumn("orgId", lower(outcomes["orgId"]))
+    
     outcomes.createOrReplaceTempView('outcomes')
-    outcomes = spark.sql(f"""
-        SELECT
-            campaign_id.oid as campaignId,
-            activityId.oid as activityId,
-            entityName,
-            fieldName,
-            kpi_name,
-            lower(orgId) AS orgId
-        FROM outcomes
+    spark.sql("""
+      MERGE INTO dg_performance_management.campaign_outcomes AS target
+      USING outcomes AS source
+      ON target.orgId = source.orgId
+      AND target.campaignId = source.campaignId
+      AND target.activityId = source.activityId
+      WHEN NOT MATCHED THEN
+        INSERT *        
     """)
-    delta_table_partition_ovrewrite(outcomes, "dg_performance_management.campaign_outcomes", ['orgId'])
