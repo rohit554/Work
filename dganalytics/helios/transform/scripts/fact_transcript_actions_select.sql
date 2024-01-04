08302790ac4da6f678c9623d0b00bf80cabded7a
Ganga_C <gchapalli@datagamz.com>
2024-01-04 18:55:46 +1100

    fact_transcript_actions script added

---
+++ dganalytics/helios/transform/scripts/fact_transcript_actions_select.sql
@@ -0,0 +1,92 @@
+SELECT
+conversationId,
+category,
+action,
+action_label,
+MIN(
+    CASE
+        WHEN line = startLine THEN from_unixtime(startTimeMs / 1000)
+    END
+    ) as startTime,
+MAX(
+    CASE
+        WHEN line = endLine THEN from_unixtime((startTimeMs + COALESCE(milliseconds, 0)) / 1000)
+    END
+    ) as endTime
+FROM
+(
+    SELECT
+        *
+    FROM
+        (
+            SELECT
+                P.conversationId,
+                category,
+                action,
+                action_label,
+                T.startTimeMs,
+                T.milliseconds,
+                T.line,
+                element_at(lines, 1) startLine,
+                element_at(lines, size(lines)) endLine
+            FROM
+            (
+                SELECT
+                    conversationId,
+                    step.category,
+                    step.action,
+                    step.action_label,
+                    split(step.line, ',') lines
+                FROM
+                    (
+                    SELECT
+                        conversationId,
+                        EXPLODE(process_map) step
+                    FROM
+                        (
+                        SELECT
+                            conversation_id conversationId,
+                            process_map,
+                            row_number() OVER(
+                            PARTITION BY conversation_id
+                            ORDER BY
+                                recordInsertTime DESC
+                            ) RN
+                        FROM
+                        gpc_{tenant}.raw_transcript_insights
+                        WHERE extractDate = '{extract_date}'
+                        )
+                    WHERE
+                        RN = 1
+                    )
+                ) P
+                JOIN (
+                    SELECT
+                        TP.conversationId,
+                        TP.text,
+                        TP.startTimeMs,
+                        TP.milliseconds,
+                        row_number() OVER (
+                        PARTITION BY TP.conversationId
+                        ORDER BY
+                            startTimeMs
+                        ) line
+                    FROM
+                        gpc_{tenant}.fact_conversation_transcript_phrases TP
+                        INNER JOIN gpc_{tenant}.raw_speechandtextanalytics_transcript C ON TP.conversationId = c.conversationId
+                        where
+                        c.extractDate = '{extract_date}'
+                ) T ON T.conversationid = P.conversationId
+            and (
+            element_at(lines, 1) = T.line
+            OR element_at(lines, size(lines)) = T.line
+            )
+        order by
+            P.conversationId
+    )
+)
+GROUP BY
+conversationId,
+category,
+action,
+action_label
