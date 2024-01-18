b700b4d81134a23c0ea99ca02bcfba7e7d6d331d
Ganga_C <gchapalli@datagamz.com>
2024-01-18 20:20:37 +1100

    dim_conversations_insert query deleted

---
+++ dganalytics/helios/transform/scripts/update_location_dim_conversations.sql
@@ -0,0 +1,13 @@
+UPDATE dgdm_{tenant}.dim_conversations C
+SET location = (
+    SELECT participant.attributes["Mailing State"] 
+    FROM (SELECT conversationId, explode(participants) participant FROM gpc_{tenant}.raw_conversation_details
+    WHERE 
+    extractDate = '{extract_date}'
+    and  extractIntervalStartTime = '{extract_start_time}' 
+    and extractIntervalEndTime = '{extract_end_time}'
+    
+    ) 
+    WHERE conversationId = C.conversationId AND participant.attributes["Mailing State"] IS NOT NULL 
+    LIMIT 1
+)
